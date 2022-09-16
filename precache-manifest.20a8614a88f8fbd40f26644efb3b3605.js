28669dc7f524ddf2f8893756c3ef8c4a96ed8fa5
Wangstaboi <bubmom9@gmail.com>
2022-09-15 22:29:01 -0700

    Updates

---
+++ precache-manifest.20a8614a88f8fbd40f26644efb3b3605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d9f8a482068b41be17",
+    "url": "/random-quote-machine/static/css/main.64eacf0f.chunk.css"
+  },
+  {
+    "revision": "23d9f8a482068b41be17",
+    "url": "/random-quote-machine/static/js/main.216fb32d.chunk.js"
+  },
+  {
+    "revision": "716bd1ca126b1f72f7b2",
+    "url": "/random-quote-machine/static/js/runtime~main.5ececeae.js"
+  },
+  {
+    "revision": "7730dcc14c2a3c67a7e8",
+    "url": "/random-quote-machine/static/js/2.1e3af171.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/random-quote-machine/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "fb83d9fa92e08c9861cb3b15b4653a27",
+    "url": "/random-quote-machine/index.html"
+  }
+];
